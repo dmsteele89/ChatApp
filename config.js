bbf96e9da8ae8a163e470a406b81ee0bf9a6d668
Steele <dsteele5@lab-ceb131-m23.eastern.ewu.edu>
2019-05-29 13:21:53 -0700

    Added updated files

--- config.js
+++ config.js
@@ -1,4 +1,4 @@
-const tokenUrl = "https://us1.pusherplatform.io/services/chatkit_token_provider/v1/3ffc92ca-a0aa-4531-b75c-eb42bb11dc77/token";
-const instanceLocator = "v1:us1:3ffc92ca-a0aa-4531-b75c-eb42bb11dc77";
+const tokenUrl = "https://us1.pusherplatform.io/services/chatkit_token_provider/v1/13a31a26-543d-4500-8e5e-886e053a8cf8/token";
+const instanceLocator = "v1:us1:13a31a26-543d-4500-8e5e-886e053a8cf8";
 
 export { tokenUrl, instanceLocator }
